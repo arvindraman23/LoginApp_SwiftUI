1e475b26ad3aa4a0a12b974881050225a8b0dea7
saimeghanareddy <saimeghanareddy5@gmail.com>
2024-10-21 20:57:59 -0500

    Initial Commit

---
+++ LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by Meghana  on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
